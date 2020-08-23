ce5e1561ea496065273ae7fa9df61a1a5af55336
theazat <sayanazat@gmail.com>
2020-08-23 15:06:24 +0300

    [DEV][master][Setting up RecyclerView Adapter]

---
+++ app/src/main/java/com/azat/newsappmvvm/ui/NewsAdapter.kt
@@ -0,0 +1,72 @@
+package com.azat.newsappmvvm.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.azat.newsappmvvm.R
+import com.azat.newsappmvvm.model.Article
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_news.view.*
+
+/*************************
+ * Created by AZAT SAYAN *
+ *                       *
+ * Contact: @theazat     *
+ *                       *
+ * 23/08/2020 - 2:42 PM  *
+ ************************/
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
+    private val differCallBack = object : DiffUtil.ItemCallback<Article>() {
+
+        override fun areItemsTheSame(oldItem: Article, newItem: Article) =
+            oldItem.url == newItem.url
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article) = oldItem == newItem
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallBack)
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        NewsViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_news,
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount() = differ.currentList.size
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            article?.let {
+                Glide.with(this).load(it.urlToImage).into(ivArticleImage)
+                tvSource.text = it.source?.name
+                tvTitle.text = it.title
+                tvDescription.text = it.description
+                tvPublishedAt.text = it.publishedAt
+
+                setOnClickListener {
+                    onItemClickListener?.let {
+                        it(article)
+                    }
+                }
+            }
+        }
+    }
+
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    inner class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+}
--- app/src/main/java/com/azat/newsappmvvm/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/azat/newsappmvvm/ui/fragments/BreakingNewsFragment.kt
@@ -1,8 +1,41 @@
 package com.azat.newsappmvvm.ui.fragments
 
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.azat.newsappmvvm.R
+import com.azat.newsappmvvm.ui.NewsAdapter
+import com.azat.newsappmvvm.ui.NewsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 
-class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news)
+@AndroidEntryPoint
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+
+    lateinit var newsAdapter: NewsAdapter
+
+    private val viewModel: NewsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpRecyclerView()
+
+        viewModel.newsLiveData.observe(viewLifecycleOwner, Observer {
+            newsAdapter.differ.submitList(it)
+        })
+    }
+
+    private fun setUpRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+}
