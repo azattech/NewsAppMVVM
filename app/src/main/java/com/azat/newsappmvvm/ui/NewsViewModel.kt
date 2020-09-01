6e8e21db2bdd11c7bc7491e622f9c570b0d7da0e
theazat <sayanazat@gmail.com>
2020-09-01 07:35:04 +0300

    [DEV][master][Some color changes]

--- app/src/main/java/com/azat/newsappmvvm/ui/NewsViewModel.kt
+++ app/src/main/java/com/azat/newsappmvvm/ui/NewsViewModel.kt
@@ -14,7 +14,7 @@ import kotlinx.coroutines.launch
  *                       *
  * Contact: @theazat     *
  *                       *
- * 23/08/2020 - 10:28 AM  *
+ * 23/08/2020 - 10:28 AM *
  ************************/
 class NewsViewModel @ViewModelInject constructor(
     private val remoteRepository: RemoteRepository,
